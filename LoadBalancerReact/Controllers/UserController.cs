90023106d200f68d758e184c0b0440584c3562b0
Kron285 <91598400+Kron285@users.noreply.github.com>
2022-11-22 17:59:05 +0200

    Add project files.

---
+++ LoadBalancerReact/Controllers/UserController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LoadBalancerReact.Controllers
+{
+    [Route("api/users")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private static int counter = 0;
+        [HttpGet]
+        public IActionResult GetUsers()
+        {
+            var remotePort = HttpContext.Connection.RemotePort;
+            var localPort = HttpContext.Connection.LocalPort;
+            var users = new[]
+            {
+                new { Name = "Oleg" },
+                new { Name = "Ivan" },
+                new { Name = localPort.ToString()},
+                new { Name = remotePort.ToString()},
+                new { Name = Environment.GetEnvironmentVariable("HOSTNAME")},
+                new { Name = (counter+1).ToString()}
+            };
+            return Ok(users);
+        }
+
+
+    }
+}
